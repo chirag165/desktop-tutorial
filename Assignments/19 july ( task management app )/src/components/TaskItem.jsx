843fa138e4df9b590958372896212d109c5bc344
chirag165 <chiragdadarwal@gmail.com>
2025-07-22 21:50:38 +0530

    Add files via upload

---
+++ Assignments/19 july ( task management app )/src/components/TaskItem.jsx
@@ -0,0 +1,15 @@
+export default function TaskItem({ task, onDelete, onToggle }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={task.completed}
+        onChange={() => onToggle(task)}
+      />
+      <span style={{ textDecoration: task.completed ? "line-through" : "none" }}>
+        {task.title}
+      </span>
+      <button onClick={() => onDelete(task.id)}>Delete</button>
+    </li>
+  );
+}
